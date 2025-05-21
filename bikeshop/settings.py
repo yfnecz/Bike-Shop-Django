6fbf43f956bf8abf6e53289a54e676fff4a6590c
Natalia Shishina <nshishina@odin.com>
2025-05-21 01:24:39 -0700

    allow

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z)%15u9lmbz^@u=7(04b94i8h#jtb4#g9ln_4z$ivuv*32z@4z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bike-shop-django.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'bike-shop-django.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ["https://bike-shop-django.onrender.com"]
 
