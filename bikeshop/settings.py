089d8227769a73857a079a35eb08586d5b922e7c
Natalia Shishina <nshishina@odin.com>
2025-05-23 18:17:06 -0700

    disable toolbar

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bike-shop-django.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', "https://bike-shop-django.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://bike-shop-django.onrender.com"]
 
 INTERNAL_IPS = [
     # ...
