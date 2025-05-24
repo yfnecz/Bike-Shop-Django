1bc21ecd3060aa3f2f204a4b6290070cbfbad73a
Natalia Shishina <nshishina@odin.com>
2025-05-23 18:10:00 -0700

    disable toolbar

--- bikeshop/urls.py
+++ bikeshop/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls'))
-] # + debug_toolbar_urls()
+] #+ debug_toolbar_urls()
